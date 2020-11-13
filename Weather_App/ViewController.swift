061071f4bfd1233255b5f98d57c277f356390bcf
joel <joelcrawford30@gmail.com>
2020-11-13 12:50:02 +0300

    initial commt

---
+++ Weather_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather_App
+//
+//  Created by JOEL CRAWFORD on 11/13/20.
+//  Copyright © 2020 JOEL CRAWFORD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Weather_App/WeatherCell.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherCell.swift
+//  Weather_App
+//
+//  Created by JOEL CRAWFORD on 11/13/20.
+//  Copyright © 2020 JOEL CRAWFORD. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var cityName: UILabel!
+    @IBOutlet weak var bookMarkBtn: UIButton!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var pressureLabel: UILabel!
+    
+}
